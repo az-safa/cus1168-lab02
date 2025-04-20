f1c07c30196dce2be22fb5566c1691b85f1e7745
az-safa <saffah.azeem23@my.stjohns.edu>
2025-04-20 17:55:36 -0400

    finished homework

--- src/main/java/academy/javapro/Main.java
+++ src/main/java/academy/javapro/Main.java
@@ -1,34 +1,46 @@
-package academy.javapro;
+package cus1168;
 
-public class Main {
+public class MainEdited {
     public static void main(String[] args) {
         // Step 1: Create a new Tesla Model S, 2024
-        // TODO: Add your code here
+    	// TODO: Add your code here
+        TeslaEdited myTesla = new TeslaEdited("Model S", 2024);
 
         // Step 2: Display initial status
         System.out.println("Initial Status:");
         // TODO: Display car's initial state
+        myTesla.displayStatus();
 
         // Step 3: Start the car and test basic vehicle functions
         System.out.println("\nTesting basic vehicle functions:");
         // TODO: Start engine
+        myTesla.startEngine();
         // TODO: Accelerate
+        myTesla.accelerate();
         // TODO: Brake
+        myTesla.brake();
 
         // Step 4: Test autonomous features
         System.out.println("\nTesting autonomous features:");
         // TODO: Enable autopilot
+        myTesla.enableAutopilot();
         // TODO: Check if autopilot is enabled
+        System.out.println("Autopilot is currently: " + (myTesla.isAutopilotEnabled() ? "enabled" : "disabled"));
         // TODO: Disable autopilot
+        myTesla.disableAutopilot();
 
         // Step 5: Test electric features
         System.out.println("\nTesting electric features:");
         // TODO: Start charging
+        myTesla.charge();
         // TODO: Check charging status
+        System.out.println("Charging status: " + myTesla.isCharging());
 
         // Step 6: Stop the car and display final status
         System.out.println("\nFinal car status:");
         // TODO: Stop engine
+        myTesla.stopEngine();
         // TODO: Display final state
+        myTesla.displayStatus();
     }
 }
